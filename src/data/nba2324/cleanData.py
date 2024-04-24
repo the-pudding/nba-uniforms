4ffb55af3d880934c3d4f6f1dd34706558a32877
tylermachado <3586466+tylermachado@users.noreply.github.com>
2024-04-23 21:26:22 -0400

    color listing

--- src/data/nba2324/cleanData.py
+++ src/data/nba2324/cleanData.py
@@ -2,6 +2,23 @@ import csv
 import json
 import webcolors
 
+def closest_color(requested_color):
+    min_colors = {}
+    for key, name in webcolors.CSS3_HEX_TO_NAMES.items():
+        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+        rd = (r_c - requested_color[0]) ** 2
+        gd = (g_c - requested_color[1]) ** 2
+        bd = (b_c - requested_color[2]) ** 2
+        min_colors[(rd + gd + bd)] = name
+    return min_colors[min(min_colors.keys())]
+
+def get_color_name(requested_color):
+    try:
+        closest_name = actual_name = webcolors.hex_to_name(requested_color)
+    except ValueError:
+        closest_name = closest_color(webcolors.hex_to_rgb(requested_color))
+    return closest_name
+
 try:
     with open('rawData.json') as f:
         data = json.load(f)
@@ -17,11 +34,11 @@ for item in data:
         for event in item["games"]:
             homeTeam = event["homeTeam"]
             homeTeamEdition = event["homeTeamEdition"]
-            homeTeamColor = 'tk'
+            homeTeamColor = get_color_name(event["homeTeamBGColor"])
             homeTeamHex = event["homeTeamBGColor"]
             awayTeam = event["awayTeam"]
             awayTeamEdition = event["awayTeamEdition"]
-            awayTeamColor = 'tk'
+            awayTeamColor = get_color_name(event["awayTeamBGColor"])
             awayTeamHex = event["awayTeamBGColor"]
             csv_data.append([gameDate, weekDay, homeTeam, homeTeamEdition, homeTeamColor, homeTeamHex, awayTeam, awayTeamEdition, awayTeamColor, awayTeamHex])
     except KeyError as e:
