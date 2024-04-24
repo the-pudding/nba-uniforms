a86095d29f88b9a0c2f5de4e350bc0dfc599413d
tylermachado <3586466+tylermachado@users.noreply.github.com>
2024-04-23 21:08:53 -0400

    update python script

--- src/data/nba2324/cleanData.py
+++ src/data/nba2324/cleanData.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import webcolors
 
 try:
     with open('rawData.json') as f:
@@ -16,11 +17,13 @@ for item in data:
         for event in item["games"]:
             homeTeam = event["homeTeam"]
             homeTeamEdition = event["homeTeamEdition"]
+            homeTeamColor = 'tk'
             homeTeamHex = event["homeTeamBGColor"]
             awayTeam = event["awayTeam"]
             awayTeamEdition = event["awayTeamEdition"]
+            awayTeamColor = 'tk'
             awayTeamHex = event["awayTeamBGColor"]
-            csv_data.append([gameDate, weekDay, homeTeam, homeTeamEdition, homeTeamHex, awayTeam, awayTeamEdition, awayTeamHex])
+            csv_data.append([gameDate, weekDay, homeTeam, homeTeamEdition, homeTeamColor, homeTeamHex, awayTeam, awayTeamEdition, awayTeamColor, awayTeamHex])
     except KeyError as e:
         print(f"Key {e} not found in the data.")
         continue
@@ -29,7 +32,7 @@ csv_file_path = "output.csv"
 try:
     with open(csv_file_path, mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(["gameDate", "weekDay", "homeTeam", "homeTeamEdition", "homeTeamHex", "awayTeam", "awayTeamEdition", "awayTeamHex"])
+        writer.writerow(["gameDate", "weekDay", "homeTeam", "homeTeamEdition", "homeTeamColor", "homeTeamHex", "awayTeam", "awayTeamEdition", "awayTeamColor", "awayTeamHex"])
         writer.writerows(csv_data)
 except IOError:
     print("Could not write to file 'output.csv'.")
