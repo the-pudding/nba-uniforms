e30f72374207e87935dd7d082fa646d563d1fd23
tylermachado <3586466+tylermachado@users.noreply.github.com>
2024-07-23 16:09:56 -0400

    copy updates

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -3,12 +3,15 @@
 	import Demo from "$components/demo/Demo.svelte";
 	import NBA from "$components/nba/NBA.svelte";
 	import WIP from "$components/helpers/WIP.svelte";
-	// import Footer from "$components/Footer.svelte";
+	import Header from "$components/Header.svelte";
+	import Footer from "$components/Footer.svelte";
 
 	// const copy = getContext("copy");
 	// const data = getContext("data");
 </script>
 
 <WIP />
+<!-- <svelte:window bind:innerWidth={w} bind:innerHeight={h} bind:scrollY/> -->
+<Header />
 <NBA />
-<!-- <Footer /> -->
+<Footer />
