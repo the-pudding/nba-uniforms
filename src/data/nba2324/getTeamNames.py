4eb2f5433256ac84c8e1a89471a79b1ccd3fda5b
tylermachado <3586466+tylermachado@users.noreply.github.com>
2024-07-29 11:17:01 -0400

    rename

--- src/data/nba2324/getTeamNames.py
+++ src/data/nba2324/getTeamNames.py
@@ -8,17 +8,14 @@ except FileNotFoundError:
     print("The file 'rawData.json' does not exist.")
     exit(1)
 
-# Get every possible team name
 teamNames = []
 for item in data:
     for event in item["games"]:
-        # Append the home and away team names only if the name does not already exist in teamNames
         if event["homeTeam"] not in teamNames:
             teamNames.append(event["homeTeam"])
         if event["awayTeam"] not in teamNames:
             teamNames.append(event["awayTeam"])
 
-# Create a json file with the team names, where for each team name is the value and the key is "team"
 teamNamesJson = []
 for team in teamNames:
     teamNamesJson.append({"team": team})
