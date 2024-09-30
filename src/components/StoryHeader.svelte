7a624cb86d9696465634596545f9eb8235bd3c60
tylermachado <3586466+tylermachado@users.noreply.github.com>
2024-09-29 21:31:38 -0400

    make jersey grid clickable

--- src/components/StoryHeader.svelte
+++ src/components/StoryHeader.svelte
@@ -1,14 +1,23 @@
 <script>
 	import { getContext } from "svelte";
+  import { selectedTeamStore } from '$stores/teamSelection';
 
 	const teams = getContext("teams");
+
+	$: selectedTeam = $selectedTeamStore;
+
+	function handleTeamSelect(event) {
+		selectedTeamStore.set(event.target.alt);
+	}
 </script>
 
 <header>
   <div class="header-grid__container">
     {#each teams as team}
 			<div class="header-grid__jersey">
-				<img src={`/assets/jerseys/${team.code}_icon.png`} alt={team.team} />
+				<button aria-label={`Select ${team.team}`} on:click={handleTeamSelect}>
+					<img src={`/assets/jerseys/${team.code}_icon.png`} alt={team.code} class={team.code === selectedTeam ? 'selected-team-jersey' : ''}/>
+				</button>
 			</div>
 		{/each}
     <div class="header-grid__title">Content Here</div>
@@ -24,8 +33,18 @@
   gap: 5px; /* Optional gap between cells */
 }
 
-.header-grid__jersey {
-  background-color: #f0f0f0; /* Example color */
+.header-grid__jersey img {
+	cursor: pointer;
+	opacity: 0.5;
+
+	&:hover,
+	&.selected-team-jersey {
+		opacity: 1;
+	}
+}
+
+.header-grid__jersey button {
+	background-color: unset;
 }
 
 .header-grid__title {
